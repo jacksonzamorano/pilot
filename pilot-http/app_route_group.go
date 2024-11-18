444ee554c63567130607396a212dbf2d07c40c1a
Jackson Zamorano <jackson@jacksonzamorano.com>
2024-11-18 16:54:55 -0700

    fix: routegroup api rnd 2

--- pilot-http/app_route_group.go
+++ pilot-http/app_route_group.go
@@ -11,7 +11,7 @@ func NewRouteGroup[RouteState any](routes ...GroupedRoute[RouteState]) *RouteGro
 }
 
 // Functions for Get, post, put, patch, delete, etc.
-func (rg *RouteGroup[RouteState]) Get(path string, handler RouteHandlerFn[RouteState], middleware ...MiddlewareFn[RouteState]) *GroupedRoute[RouteState] {
+func GetRoute[RouteState RouteStateCompatible](path string, handler RouteHandlerFn[RouteState], middleware ...MiddlewareFn[RouteState]) *GroupedRoute[RouteState] {
 	return &GroupedRoute[RouteState]{
 		Route:      path,
 		Method:     Get,
@@ -19,41 +19,37 @@ func (rg *RouteGroup[RouteState]) Get(path string, handler RouteHandlerFn[RouteS
 		Middleware: middleware,
 	}
 }
-func (rg *RouteGroup[RouteState]) Post(path string, handler RouteHandlerFn[RouteState], middleware ...MiddlewareFn[RouteState]) *RouteGroup[RouteState] {
-	rg.Routes = append(rg.Routes, GroupedRoute[RouteState]{
+func PostRoute[RouteState RouteStateCompatible](path string, handler RouteHandlerFn[RouteState], middleware ...MiddlewareFn[RouteState]) *GroupedRoute[RouteState] {
+	return &GroupedRoute[RouteState]{
 		Route:      path,
 		Method:     Post,
 		Handler:    handler,
 		Middleware: middleware,
-	})
-	return rg
+	}
 }
-func (rg *RouteGroup[RouteState]) Put(path string, handler RouteHandlerFn[RouteState], middleware ...MiddlewareFn[RouteState]) *RouteGroup[RouteState] {
-	rg.Routes = append(rg.Routes, GroupedRoute[RouteState]{
+func PutRoute[RouteState RouteStateCompatible](path string, handler RouteHandlerFn[RouteState], middleware ...MiddlewareFn[RouteState]) *GroupedRoute[RouteState] {
+	return &GroupedRoute[RouteState]{
 		Route:      path,
 		Method:     Put,
 		Handler:    handler,
 		Middleware: middleware,
-	})
-	return rg
+	}
 }
-func (rg *RouteGroup[RouteState]) Patch(path string, handler RouteHandlerFn[RouteState], middleware ...MiddlewareFn[RouteState]) *RouteGroup[RouteState] {
-	rg.Routes = append(rg.Routes, GroupedRoute[RouteState]{
+func PatchRoute[RouteState RouteStateCompatible](path string, handler RouteHandlerFn[RouteState], middleware ...MiddlewareFn[RouteState]) *GroupedRoute[RouteState] {
+	return &GroupedRoute[RouteState]{
 		Route:      path,
 		Method:     Patch,
 		Handler:    handler,
 		Middleware: middleware,
-	})
-	return rg
+	}
 }
-func (rg *RouteGroup[RouteState]) Delete(path string, handler RouteHandlerFn[RouteState], middleware ...MiddlewareFn[RouteState]) *RouteGroup[RouteState] {
-	rg.Routes = append(rg.Routes, GroupedRoute[RouteState]{
+func DeleteRoute[RouteState RouteStateCompatible](path string, handler RouteHandlerFn[RouteState], middleware ...MiddlewareFn[RouteState]) *GroupedRoute[RouteState] {
+	return &GroupedRoute[RouteState]{
 		Route:      path,
 		Method:     Delete,
 		Handler:    handler,
 		Middleware: middleware,
-	})
-	return rg
+	}
 }
 
 
