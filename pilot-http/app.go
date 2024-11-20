894b860b711ff0c2bc214e40aac9a0ae578c87e8
Jackson Zamorano <jackson@jacksonzamorano.com>
2024-11-19 18:42:21 -0700

    feat: add better multitasking support

--- pilot-http/app.go
+++ pilot-http/app.go
@@ -9,6 +9,7 @@ import (
 	"os"
 	"os/signal"
 	"strings"
+	"sync"
 
 	"github.com/jackc/pgx/v5/pgxpool"
 )
@@ -28,7 +29,28 @@ type Application[RouteState RouteStateCompatible] struct {
 	WorkerCount      int32
 }
 
-func NewApplication[RouteState any](port string, cfg DatabaseConfiguration, middlewareFn func(*HttpRequest) *RouteState, ctx context.Context) *Application[RouteState] {
+func NewInlineApplication[RouteState any](port string, cfg DatabaseConfiguration, middlewareFn func(*HttpRequest) *RouteState, ctx context.Context) *Application[RouteState] {
+	pool, err := pgxpool.New(ctx, cfg.GetConnectionString())
+	if err != nil {
+		panic(err)
+	}
+
+	return &Application[RouteState]{
+		Port:             port,
+		CorsOrigin:       "*",
+		CorsHeaders:      "*",
+		CorsMethods:      "GET, PUT, POST, DELETE, HEAD",
+		Routes:           NewRouteCollection[RouteState](),
+		SilentMode:       false,
+		Database:         pool,
+		GlobalMiddleware: middlewareFn,
+		WorkerCount:      10,
+		Context:          ctx,
+	}
+}
+
+func NewApplication[RouteState any](port string, cfg DatabaseConfiguration, middlewareFn func(*HttpRequest) *RouteState) *Application[RouteState] {
+	ctx, _ := signal.NotifyContext(context.Background(), os.Interrupt, os.Kill)
 	pool, err := pgxpool.New(ctx, cfg.GetConnectionString())
 	if err != nil {
 		panic(err)
@@ -74,10 +96,15 @@ func (a *Application[RouteState]) Start() {
 	if err != nil {
 		panic(err)
 	}
+	var wg sync.WaitGroup
 	queue := make(chan net.Conn, a.WorkerCount*10)
 	recvQueue := make(chan net.Conn, a.WorkerCount*10)
 	for i := int32(0); i < a.WorkerCount; i++ {
-		go handleRequest(queue, a, (*a).Context)
+		wg.Add(1)
+		go func() {
+			handleRequest(queue, a, (*a).Context)
+			wg.Done()
+		}()
 	}
 	go func() {
 		for {
@@ -96,8 +123,9 @@ func (a *Application[RouteState]) Start() {
 		for {
 			select {
 			case <-(*a).Context.Done():
-				log.Println("Stopping server...")
+				log.Println("Stopping Pilot server...")
 				listener.Close()
+				wg.Wait()
 				return
 			case conn := <-recvQueue:
 				queue <- conn
@@ -114,13 +142,13 @@ func handleRequest[RouteState any](conn <-chan net.Conn, app *Application[RouteS
 	for {
 		select {
 		case <-context.Done():
-			log.Println("Stopping worker...")
 			db.Release()
+			log.Println("Pilot worker stopped.")
 			return
 		case conn := <-conn:
 			request := ParseRequest(&conn)
 			if request == nil {
-				return
+				continue
 			}
 
 			response := StringResponse("")
@@ -129,17 +157,17 @@ func handleRequest[RouteState any](conn <-chan net.Conn, app *Application[RouteS
 			response.ApplyCors(&app.CorsOrigin, &app.CorsHeaders, &app.CorsMethods)
 			if request.Method == Options {
 				response.Write(conn)
-				return
+				continue
 			}
 			route := (*app).Routes.FindPath(request.Path, false)
 			if route == nil {
 				response.Write(conn)
-				return
+				continue
 			}
 			handler, found := route.Handlers[request.Method]
 			if !found {
 				response.Write(conn)
-				return
+				continue
 			}
 
 			routeState := (app.GlobalMiddleware)(request)
@@ -149,7 +177,7 @@ func handleRequest[RouteState any](conn <-chan net.Conn, app *Application[RouteS
 				if response != nil {
 					response.ApplyCors(&app.CorsOrigin, &app.CorsHeaders, &app.CorsMethods)
 					response.Write(conn)
-					return
+					continue
 				}
 			}
 
@@ -161,8 +189,4 @@ func handleRequest[RouteState any](conn <-chan net.Conn, app *Application[RouteS
 			response.Write(conn)
 		}
 	}
-}
-
-func DefaultContext() (context.Context, context.CancelFunc) {
-	return signal.NotifyContext(context.Background(), os.Interrupt, os.Kill)
 }
